4df583b05fbf45de61943879a077e595076b9376
von Rudorff, Guido Falk <guido@vonrudorff.de>
2022-08-24 15:52:33 +0200

    Connect MongoDB

--- src/server/api/app/main.py
+++ src/server/api/app/main.py
@@ -1,24 +1,28 @@
-from unicodedata import name
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, constr
 from typing import List, Optional
+import pymongo
+import os
 
 counter = 0
 
 from fastapi import FastAPI
 
 app = FastAPI()
+db = pymongo.MongoClient(os.getenv("MONGODB_CONNSTR"))
 
 
 class NodeAuth(BaseModel):
-    nodeid: str = Field(..., description="User-generated identifier of the node.")
-    ownerid: str = Field(..., description="System-generated identifier of the owner.")
-    computesecret: str = Field(..., description="Authentication for node.")
+    node_id: str = Field(..., description="User-generated identifier of the node.")
+    owner_token: str = Field(
+        ..., description="System-generated identifier of the owner."
+    )
+    compute_token: str = Field(..., description="Authentication for node.")
 
 
 class UserAuth(BaseModel):
-    userid: str = Field(..., description="System-generated identifier of the user.")
-    projectid: Optional[str] = Field(
-        ..., description="System-generated identifier of a project."
+    user_token: str = Field(..., description="System-generated identifier of the user.")
+    collaboration_token: Optional[str] = Field(
+        ..., description="System-generated identifier of a collaboration."
     )
 
 
@@ -28,28 +32,29 @@ class JobFetch(NodeAuth):
     memory_mb: int = Field(..., description="Available memory in MB.")
 
 
-class JobSpec(BaseModel):
-    container: str = Field(..., description="URL of the container to run.")
-    command: str = Field(..., description="Command to run.")
-    datasource: str = Field(
-        ..., description="URL, e.g. to S3 directory containing the data to be present."
+class JobSpec(UserAuth):
+    container: str = Field(..., description="Name of the container to run.")
+    command: constr = Field(..., description="Command to run.", max_length=2000)
+    data_source: str = Field(
+        ..., description="S3 directory containing the data to be present."
     )
-    datatarget: str = Field(
+    data_target: str = Field(
         ...,
-        description="URL, e.g. to S3 directory where all modified files should be uploaded to.",
+        description="S3 directory where all modified files should be uploaded to.",
     )
     core_limit: int = Field(..., description="Requested number of cores.")
     memory_mb_limit: int = Field(..., description="Requested memory in MB.")
+    time_seconds_limit: int = Field(..., description="Requested wall time duration.")
     tags: Optional[List[str]] = Field(
         ...,
-        description="List of tags this calculation belongs to. Unique within a project or (if no project is specified) for a user.",
+        description="List of tags this calculation belongs to. Unique within a collaboration or (if no collaboration is specified) for a user.",
     )
-    jobid: str = Field(..., description="System-generated job identifier.")
 
 
 @app.post("/heartbeat")
 def register_heartbeat(body: NodeAuth):
     # TODO: write to mongodb
+    # TODO: rreturn challenge for signed communication
     pass
 
 
@@ -63,4 +68,10 @@ def job_fetch(body: JobFetch):
 
 @app.post("/job/create")
 def job_create(body: JobSpec):
+    pass
+
+
+@app.post("/owner/activate")
+def owner_activate():
+    # TODO: initial public key load, only works once
     pass
