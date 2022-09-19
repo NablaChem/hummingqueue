7c4ace36fceb3c03911b1495422d5afe30247474
von Rudorff, Guido Falk <guido@vonrudorff.de>
2022-09-19 13:12:07 +0200

    Split out owner endpoints

--- src/server/api/app/main.py
+++ src/server/api/app/main.py
@@ -10,13 +10,12 @@ from . import auth
 
 counter = 0
 
-from fastapi import FastAPI, Header, Request
+from fastapi import FastAPI, Request
 
 app = FastAPI()
+from .routers import owner
 
-
-class AdminAuth(BaseModel):
-    admin_token: str = Field(..., description="Installation-specific admin token.")
+app.include_router(owner.app)
 
 
 class NodeAuth(BaseModel):
@@ -90,56 +89,3 @@ def job_fetch(body: JobFetch):
 @app.post("/job/create")
 def job_create(body: JobSpec):
     pass
-
-
-class OwnerCreateResponse(BaseModel):
-    owner_token: str = Field(..., description="Newly created owner token.")
-
-
-@app.post(
-    "/owner/create",
-    summary="Creates a new owner.",
-    description="Only accessible to the admin of the installation.",
-    tags=["authentication"],
-    responses={
-        403: {"description": "Authentication failed."},
-        200: {"description": "Owner created.", "model": OwnerCreateResponse},
-    },
-)
-def owner_create(body: AdminAuth):
-    # validate request
-    validators.is_admin(body)
-
-    # handle request
-    owner_token = helpers.new_token()
-    auth.db.users.insert_one({"token": owner_token, "is_owner": True})
-    return {"owner_token": owner_token}
-
-
-class OwnerFirstTimeAuth(BaseModel):
-    owner_token: str = Field(..., description="Owner token.")
-    public_key: str = Field(..., description="Base64-encoded DER format public key.")
-
-
-@app.post(
-    "/owner/activate",
-    summary="Initial setup for a new owner.",
-    description="The user-supplied public key will be used for future authentication of this owner. Can only be called once for a given owner.",
-    tags=["authentication"],
-    responses={
-        404: {"description": "No such owner."},
-        403: {"description": "Authentication error."},
-        200: {"description": "Public key stored."},
-    },
-)
-def owner_activate(body: OwnerFirstTimeAuth, request: Request):
-    # validate request
-    owner = validators.owner_exists(body)
-    validators.owner_has_no_key(owner)
-    validators.public_key_consistent(body, request)
-
-    # handle request
-    auth.db.users.update_one(
-        {"owner_token": body.owner_token, "is_owner": True},
-        {"public_key": body.public_key},
-    )
