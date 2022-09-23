a8443682ec99c4a0d4f0c0d6c9968a31195a794f
von Rudorff, Guido Falk <guido@vonrudorff.de>
2022-09-23 08:58:31 +0200

    CLI testing in container

--- src/user/hmq/test_hmq.py
+++ src/user/hmq/test_hmq.py
@@ -1,2 +1,16 @@
-def test_foo():
-    assert True
+from http.client import responses
+from click.testing import CliRunner
+from . import hmq
+import requests
+
+
+def test_hello_world():
+    runner = CliRunner()
+    response = requests.post(
+        "http://api.lvh.me/owner/create", json={"admin_token": "admin"}
+    )
+    assert response.status_code == 200
+    owner = response.json()["owner_token"]
+    result = runner.invoke(hmq.cli, ["owner-init", "http://api.lvh.me", owner])
+    assert "Public key set up" in result.output
+    assert result.exit_code == 0
