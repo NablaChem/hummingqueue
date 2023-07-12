8a315521594402e62fe3f6b30c18f3bae855894d
von Rudorff, Guido Falk <guido@vonrudorff.de>
2023-07-12 11:30:26 +0200

    Split view

--- src/dashboard/app/dashboard.py
+++ src/dashboard/app/dashboard.py
@@ -1,12 +1,15 @@
 import streamlit as st
 import pandas as pd
 import requests
+from streamlit_autorefresh import st_autorefresh
 
 st.set_page_config(page_title="Hummingqueue Dashboard", layout="wide")
+# refresh every minute
+st_autorefresh(interval=60 * 1000)
 
 st.title("Hummingqueue Dashboard")
 col1, col2 = st.columns(2)
-col1.caption("Queue depth")
+
 
 result = requests.get("http://hmq/usage/inspect")
 
@@ -15,10 +18,12 @@ for minutes_ago, data in result.json().items():
     data["age"] = int(minutes_ago)
     rows.append(data)
 df = pd.DataFrame(rows)
-df = df.sort_values("age")
-
-
-col1.line_chart(df, x="age")
+df = df.sort_values("age", ascending=False)
+col1.caption("Compute resources")
+col1.line_chart(df, x="age", y=["cores_available", "cores_used"])
+col1.caption("Queue depth")
+col2.line_chart(df, x="age", y=["tasks_queued"])
+col2.line_chart(df, x="age", y=["tasks_running"])
 # col2.caption("Tags")
 # for tag in "foo bar sdjfhwsökgdjhsökgj".split():
 #    col2.progress(0.54, tag)#
