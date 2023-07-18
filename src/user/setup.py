b33e4e6d2ce9a28a0ed40035fbb671c3924ebde0
von Rudorff, Guido Falk <guido@vonrudorff.de>
2023-07-18 15:45:20 +0200

    Publish

--- src/user/setup.py
+++ src/user/setup.py
@@ -1,12 +1,23 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
 setup(
-    name='hmq',
+    name="hmq",
+    version="0.1",
+    description="Distributed computing.",
+    url="https://github.com/NablaChem/hummingqueue",
+    author="Guido Falk von Rudorff",
+    author_email="guido@vonrudorff.de",
+    license="AGPL-3.0",
+    packages=["hmq"],
+    zip_safe=False,
     install_requires=[
-        'click',
+        "dnspython",
+        "requests",
+        "cloudpickle",
+        "pynacl",
+        "tqdm",
+        "rq",
+        "toml",
+        "hmq",
     ],
-    entry_points={
-        'console_scripts': [
-            'hmq=hmq.hmq:cli'
-        ],
-    },
 )
