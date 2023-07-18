3038116e3d3de0aea84a2b53edf525526831bb58
von Rudorff, Guido Falk <guido@vonrudorff.de>
2023-07-18 15:45:42 +0200

    Auto-create python envs

--- src/computenode/director.py
+++ src/computenode/director.py
@@ -1,4 +1,6 @@
 import hmq
+import subprocess
+import shlex
 import socket
 import time
 import toml
@@ -6,6 +8,20 @@ import sys
 import os
 from string import Template
 
+def install(config, pyver):
+    installfile=f"{config["datacenter"]["tmpdir"]}/install.sh"
+    with open(installfile) as fh:
+        fh.write(f'''#!/bin/bash
+export MAMBA_ROOT_PREFIX={config["datacenter"]["envs"]}
+eval "$({config["datacenter"]["binaries"]}/micromamba shell hook --shell bash )"
+micromamba create -n hmq_{pyver}
+micromamba activate hmq_{pyver}
+micromamba install python={pyver} -c conda-forge -y
+pip install hmq
+''')
+    subprocess.run(shlex.split(f"chmod +x {installfile}"))
+    subprocess.run(shlex.split(installfile))
+    os.remove(installfile)
 
 def main():
     # test for conda env name
@@ -37,16 +53,22 @@ def main():
         f.write(content)
 
     # run traefik
-    # os.system(f"{config['datacenter']['binaries']}/traefik --configfile {filename} &")
+    cmd = f"{config['datacenter']['binaries']}/traefik --config traefik.yml"
+    p = subprocess.Popen(shlex.split(cmd), cwd=config["datacenter"]["tmpdir"])
 
     # setup tunnel
-    # hmq.use_tunnel(config["gateway"]["address"], config["server"]["baseurl"])
+    hmq.use_tunnel(config["gateway"]["address"], config["server"]["baseurl"])
 
     # request jobs / run heartbeat
     with open(f"{sys.argv[1]}/hmq.job") as fh:
         templatestr = fh.read()
 
     while True:
+        time.sleep(10)
+
+        aggregates = dict()
+        pyvers = []
+
         # get jobs
         qs = hmq.api.has_jobs(config["datacenter"]["name"])
         for q in qs:
@@ -60,13 +82,36 @@ def main():
                 "gatewayport": config["gateway"]["port"],
                 "binaries": config["datacenter"]["binaries"],
             }
-            print(Template(templatestr).substitute(variables))
+            pyvers.append(q["version"])
+            key = f"{q['version']}-{q['numcores']}"
+            if key not in aggregates:
+                aggregates[key] = variables
+            else:
+                aggregates[key]["queues"] += " " + q["name"]
 
-        # check python env exists, build it otherwise
-        # upgrade hmq in env
+        # install python versions
+        for pyver in set(pyvers):
+            if os.path.exists(f"{config['datacenter']['envs']}/envs/hmq_{pyver}"):
+                continue
 
-        # sleep
-        time.sleep(10)
+            install(pyver)
+
+        # test how many jobs are already queued
+        output = subprocess.check_output(shlex.split("squeue -u $USER"), shell=True)
+        njobs = len(output.splitlines()) - 1
+        if njobs >= config["datacenter"]["maxjobs"]:
+            continue
+
+        # submit jobs
+        for variables in aggregates.values():
+            with open(f"{config['datacenter']['tmpdir']}/hmq.job", "w") as fh:
+                content = Template(templatestr).substitute(variables)
+                fh.write(content)
+
+            subprocess.run(shlex.split("sbatch hmq.job"), cwd=config["datacenter"]["tmpdir"])
+            njobs += 1
+            if njobs >= config["datacenter"]["maxjobs"]:
+                break
 
 
 if __name__ == "__main__":
