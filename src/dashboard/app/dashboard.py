91009adfacf7ad4828d27bb9677d94c07cb9c5a8
von Rudorff, Guido Falk <guido@vonrudorff.de>
2025-03-11 15:52:32 +0100

    Show global stats

--- src/dashboard/app/dashboard.py
+++ src/dashboard/app/dashboard.py
@@ -32,86 +32,6 @@ st.title("Hummingqueue Dashboard")
 st.text("The data is updated once a minute.")
 queue, datacenters = st.tabs(["Queue", "Datacenters"])
 
-result = requests.get("http://hmq/queue/inspect")
-
-rows = []
-for minutes_ago, data in result.json().items():
-    data["age"] = -int(minutes_ago)
-    rows.append(data)
-df = pd.DataFrame(rows)
-df = df.sort_values("age", ascending=False)
-
-
-used = (
-    alt.Chart(df)
-    .mark_circle(opacity=0.6, color=primary)
-    .mark_line(opacity=0.6, color=primary)
-    .encode(x="age", y="cores_used")
-    .properties(height=200)
-)
-domain = max(10, df.cores_allocated.max())
-allocated = (
-    alt.Chart(df)
-    .mark_area(opacity=0.6, color=primary)
-    .encode(
-        x="age",
-        y=alt.Y("cores_allocated", scale=alt.Scale(domain=(0, domain))).axis(
-            titleColor=primary,
-            title="Allocated / used cores",
-        ),
-    )
-)
-
-available = (
-    alt.Chart(df)
-    .mark_line(color=secondary)
-    .encode(
-        x="age",
-        y=alt.Y("cores_available").axis(titleColor=secondary, title="Available cores"),
-    )
-)
-c = alt.layer(allocated + used, available).resolve_scale(y="independent")
-
-with queue:
-    st.altair_chart(
-        c,
-        use_container_width=True,
-    )
-
-domain = max(10, df.tasks_running.max())
-tasks = (
-    alt.Chart(df)
-    .mark_line(color=primary)
-    .encode(
-        x="age",
-        y=alt.Y("tasks_running", scale=alt.Scale(domain=(0, domain))).axis(
-            titleColor=primary
-        ),
-    )
-    .properties(height=200)
-)
-domain = max(10, df.tasks_queued.max())
-tasks2 = (
-    alt.Chart(df)
-    .mark_line(color=secondary)
-    .encode(
-        x="age",
-        y=alt.Y("tasks_queued", scale=alt.Scale(domain=(0, domain))).axis(
-            titleColor=secondary
-        ),
-    )
-)
-
-c = alt.layer(tasks, tasks2).resolve_scale(y="independent")
-c.layer[0].encoding.y.title = "Running tasks"
-c.layer[1].encoding.y.title = "Queued tasks"
-c.layer[0].encoding.x.title = "Time ago [minutes]"
-with queue:
-    st.altair_chart(
-        c,
-        use_container_width=True,
-    )
-
 
 def T(x):
     return human_readable.time_delta(dt.timedelta(seconds=x)) + " ago"
@@ -147,6 +67,107 @@ def T2(x):
     return f"{seconds}s"
 
 
+result = requests.get("http://hmq/queue/inspect")
+
+rows = []
+for minutes_ago, data in result.json().items():
+    data["age"] = -int(minutes_ago)
+    rows.append(data)
+df = pd.DataFrame(rows)
+df = df.sort_values("age", ascending=False)
+
+try:
+    total_coretime = next(df.query("age == 0").iterrows())[1]["total_coretime"]
+    total_jobs = next(df.query("age == 0").iterrows())[1]["total_jobs"]
+except:
+    total_coretime = 0
+    total_jobs = 0
+used = (
+    alt.Chart(df)
+    .mark_circle(opacity=0.6, color=primary)
+    .mark_line(opacity=0.6, color=primary)
+    .encode(x="age", y="cores_used")
+    .properties(height=200)
+)
+domain = max(10, df.cores_allocated.max())
+allocated = (
+    alt.Chart(df)
+    .mark_area(opacity=0.6, color=primary)
+    .encode(
+        x="age",
+        y=alt.Y("cores_allocated", scale=alt.Scale(domain=(0, domain))).axis(
+            titleColor=primary,
+            title="Allocated / used cores",
+        ),
+    )
+)
+
+available = (
+    alt.Chart(df)
+    .mark_line(color=secondary)
+    .encode(
+        x="age",
+        y=alt.Y("cores_available").axis(titleColor=secondary, title="Available cores"),
+    )
+)
+c = alt.layer(allocated + used, available).resolve_scale(y="independent")
+
+with queue:
+    main, metric = st.columns([0.9, 0.1])
+    with main:
+        st.altair_chart(
+            c,
+            use_container_width=True,
+        )
+    with metric:
+        st.metric(
+            "Total CPU hours",
+            f"{total_coretime / 3600 / 1e6:.2f} M",
+            border=False,
+        )
+
+domain = max(10, df.tasks_running.max())
+tasks = (
+    alt.Chart(df)
+    .mark_line(color=primary)
+    .encode(
+        x="age",
+        y=alt.Y("tasks_running", scale=alt.Scale(domain=(0, domain))).axis(
+            titleColor=primary
+        ),
+    )
+    .properties(height=200)
+)
+domain = max(10, df.tasks_queued.max())
+tasks2 = (
+    alt.Chart(df)
+    .mark_line(color=secondary)
+    .encode(
+        x="age",
+        y=alt.Y("tasks_queued", scale=alt.Scale(domain=(0, domain))).axis(
+            titleColor=secondary
+        ),
+    )
+)
+
+c = alt.layer(tasks, tasks2).resolve_scale(y="independent")
+c.layer[0].encoding.y.title = "Running tasks"
+c.layer[1].encoding.y.title = "Queued tasks"
+c.layer[0].encoding.x.title = "Time ago [minutes]"
+
+with queue:
+    main, metric = st.columns([0.9, 0.1])
+    with main:
+        st.altair_chart(
+            c,
+            use_container_width=True,
+        )
+    with metric:
+        st.metric(
+            "Total tasks",
+            f"{total_jobs  / 1e6:.2f} M",
+            border=False,
+        )
 # tags
 result = requests.get("http://hmq/tags/inspect")
 df = pd.DataFrame(result.json())
